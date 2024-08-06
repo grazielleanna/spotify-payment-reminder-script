b247d5a7b36f8bb446b6036b556a334c047cf380
Grazielle <grazielleconceicao@yahoo.com.br>
2024-08-06 20:30:25 -0300

    fix: .bat file removed

--- spotify-automation.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo %DATE% %TIME% > C:\Users\grazi\projects\spotify-automation\logs\log.txt
-echo Running script... >> C:\Users\grazi\projects\spotify-automation\logs\log.txt
-"C:\Users\grazi\projects\spotify-automation\venv\Scripts\python.exe" "C:\Users\grazi\projects\spotify-automation\main.py" >> C:\Users\grazi\projects\spotify-automation\logs\log.txt 2>&1
-echo Script finished with exit code %ERRORLEVEL% >> C:\Users\grazi\projects\spotify-automation\logs\log.txt
